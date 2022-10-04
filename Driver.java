a16e38ee125cfe0e439ae90e103784cdd786a074
Alex <alexandermotormail@gmail.com>
2022-10-04 02:00:03 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,25 @@
+import java.io.File;
 public class Driver { 
  public static void main(String [] args) { 
-  Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
-  double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
-  Polynomial s = p1.add(p2); 
-  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-  if(s.hasRoot(1)) 
-   System.out.println("1 is a root of s"); 
-  else 
-   System.out.println("1 is not a root of s"); 
+    double[] a1 = {3, 2, -3, 5};
+    int[] a2 = {7, 0, 1, 7};
+    double[] b1 = {5, 9, -10, 6};
+    int[] b2 = {2, 5, 0, 3};
+
+    /* double[] a1 = {-1, -2};
+    int[] a2 = {0, 1};
+    double[] b1 = {1, 2};
+    int[] b2 = {1, 0}; */
+        
+    Polynomial a = new Polynomial(a1, a2);
+    Polynomial b = new Polynomial(b1, b2);
+    int val = 2;
+    System.out.println("Result for x = " + val + ": " + a.evaluate(val));
+    Polynomial c = a.multiply(b);               // Multiply a*b
+    c.print_poly();                             // Print a*b
+    c.saveToFile("poly.txt");             // Save c to file
+    File f = new File("poly.txt");    // Read file with c
+    Polynomial d = new Polynomial(f);          // Construct d from file
+    d.print_poly();                            // Print d
  } 
 } 
--- Polynomial.java
+++ Polynomial.java
@@ -1,42 +1,162 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 class Polynomial {
     double[] coef;
+    int[] exp;
     public Polynomial() {
         coef = new double[1];
+        exp = new int[1];
         coef[0] = 0;
+        exp[0] = 0;
     }
-    public Polynomial(double[] poly) {
-        coef = new double[poly.length];
-        System.arraycopy(poly, 0, coef, 0, poly.length);
+    public Polynomial(double[] poly, int[] deg) {
+        int len = poly.length;
+        coef = new double[len];
+        exp = new int[len];
+        System.arraycopy(poly, 0, coef,0, len);
+        System.arraycopy(deg, 0, exp, 0, len);
+    }
+
+    public Polynomial(File file) {
+        try {
+            Scanner pfile = new Scanner(file);
+            if (!(pfile.hasNextLine())) {           // Empty file case
+                coef = new double[1];
+                exp = new int[1];
+                coef[0] = 0;
+                exp[0] = 0;
+                pfile.close();
+                return;
+            }
+            String data = pfile.nextLine();
+            int d_len = data.length();
+            char[] input = new char[d_len];
+            input = data.toCharArray();
+            int len = 0;
+            for (int i = 0; i < d_len; i++) {
+                if (input[i] == '+' || input[i] == '-') len++; // Count the number of terms
+            }
+            if (input[0] != '-') len++;
+            String[] terms = new String[len];
+            int pos = 0;
+            if (input[0] == '-') {
+                terms[0] = "-";
+                pos++;
+            } else {
+                terms[0] = "";
+            }
+            for (int i = 0; i < len; i++) {
+                while (pos < d_len && input[pos] != '-' && input[pos] != '+') {
+                    terms[i] += String.valueOf(input[pos]);
+                    pos++;
+                }
+                if (pos < d_len) {
+                    if (input[pos] == '-' && i+1 < len) {
+                        terms[i+1] = "-";
+                    } else if (input[pos] == '+' && i+1 < len) {
+                        terms[i+1] = "";
+                    }
+                    pos++;
+                }
+            }            
+            coef = new double[len];
+            exp = new int[len];
+            for (int i = 0; i < len; i++) {
+                if (terms[i].contains("x")) { 
+                    String[] args = terms[i].split("x");
+                    for (int j = 0; j < args.length; j++) {
+                    }
+                    coef[i] = Double.parseDouble(args[0]);
+                    exp[i] = Integer.parseInt(args[1]);
+                } else {
+                    coef[i] = Double.parseDouble(terms[i]);
+                    exp[i] = 0;
+                }
+            }
+            pfile.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File was not found.");
+            e.printStackTrace();
+        }
     }
     public Polynomial add(Polynomial poly) {
+        if (this == null || poly == null) return null;
         int lenc = coef.length;
         int lenp = poly.coef.length;
-        double[] sum;
-        if (lenc > lenp) { // If the calling polynomial is of higher degree
-            sum = new double[lenc];
-            System.arraycopy(coef, 0, sum, 0, lenc);
-            for (int x = 0; x < lenp; x++) {
-                sum[x] += poly.coef[x];
-            }
-        } else { // If the poly taken as argument is higher or equal degree
-            sum = new double[lenp];
-            System.arraycopy(poly.coef, 0, sum, 0, lenp);
-            for (int x = 0; x < lenc; x++) {
-                sum[x] += coef[x];
+        int degc = 0;                               // Get highest degree of calling poly
+        for (int i = 0; i < lenc; i++) {
+            if (degc < exp[i]) {
+                degc = exp[i];
             }
         }
-        return new Polynomial(sum);
+        int degp = 0;                               // Get highest degree of argument poly
+        for (int i = 0; i < lenp; i++) {
+            if (degp < poly.exp[i]) {
+                degp = poly.exp[i];
+            }
+        }
+        int mlen = Math.max(degc, degp) + 1;        // Get highest degree between the two (add 1 because I want to include degree 0 into the length)
+        int[] terms = new int[mlen];                // Create array to track the terms (filled with 0s)
+        for (int i = 0; i < lenc; i++) {            // Add the coefficients from calling poly
+            terms[exp[i]] += coef[i];
+        }
+        for (int i = 0; i < lenp; i++) {            // Add the coefficients from arg poly
+            terms[poly.exp[i]] += poly.coef[i];
+        }
+        int num_terms = 0;                          // Number of terms in the post-sum poly
+        for (int i = 0; i < mlen; i++) {            // Count the number of non zero terms
+            if (terms[i] != 0) 
+                num_terms++;
+        }
+        double[] sumc = new double[num_terms];      // Array for the summed coefficients
+        int[] sume = new int[num_terms];            // Array for the summed exponents
+        int counter = 0;                            // Count the number of added terms
+        for (int i = 0; i < mlen; i++) {            // Fill the new arrays with values
+            if (terms[i] != 0) {                    // If this term is non-zero:
+                sumc[counter] = terms[i];           // summed_coef_array[current number of terms added] = coef from degree i
+                sume[counter] = i;                  // summed_exp_array[current number of terms added] = degree i
+                counter++;
+            }
+        }
+        return new Polynomial(sumc, sume);
+    }
+
+    public Polynomial multiply(Polynomial poly) {
+        int lenc = coef.length;
+        int lenp = poly.coef.length;
+        Polynomial[] products = new Polynomial[lenp];   // Array of the polynomials made from multiplying poly1 (calling poly) by each of the terms of the poly2 (argument poly)
+        for (int i = 0; i < lenp; i++) {                // Loop through the list of p1 multiplied by each of the terms of p2
+            double[] pcoef = new double[lenc];          // Make the list to store multiplied coefficients
+            int[] pexp = new int[lenc];                 // Make the list to store multiplied exponents
+            System.arraycopy(coef, 0, pcoef,0, lenc);   // Fill the list with p1
+            System.arraycopy(exp, 0, pexp, 0, lenc);    // Fill the list with p1
+            for (int j = 0; j < lenc; j++) {            // Loop through the terms of p1 nad multiply each one by term i of p2
+                pcoef[j] *= poly.coef[i];               // Multiply the coefficients
+                pexp[j] += poly.exp[i];                 // Add the exponents (cuz you're multiplying them)
+            }
+            products[i] = new Polynomial(pcoef, pexp);
+        }
+        System.out.println();
+        Polynomial result = new Polynomial();
+        for (int i = 0; i < lenp; i++) {
+            result = result.add(products[i]);
+        }
+        return result;
     }
 
     public double evaluate(double val) {
-        int len = coef.length;
-        double sum = 0;
-        for (int x = 0; x < len; x++) {
-            double exp = 1;
-            for (int y = 0; y < x; y++) {
+        int len = coef.length;              // Take length of our poly
+        double sum = 0;                     // Sum will be added sequentially by terms of different degrees
+        for (int x = 0; x < len; x++) {     // Loop through the terms
+            double exp = 1;                 // Variable to calculate the val^(exponent)
+            for (int y = 0; y < this.exp[x]; y++) {     // Loop to do the exponentiation
                 exp *= val;
             }
-            sum += coef[x]*exp;
+            sum += coef[x]*exp;             // Add coefficient*exp to sum
         }
         return sum;
     }
@@ -44,5 +164,50 @@ class Polynomial {
     public boolean hasRoot(double x) {
         if (this.evaluate(x) == 0) return true;
         return false;
+    }
+
+    public void saveToFile(String name) {
+        try {
+            File file = new File(name);
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            }
+            FileWriter f = new FileWriter(name);
+            String poly = "";
+            for (int i = 0; i < coef.length; i++) {
+                if (exp[i] == 0) {
+                    if (i != 0 && coef[i] >= 0) {
+                        poly += "+";
+                    }
+                    poly += Double.toString(coef[i]);
+                } else {
+                    if (i != 0 && coef[i] >= 0) {
+                        poly += "+";
+                    }
+                    poly += Double.toString(coef[i]);
+                    poly += "x";
+                    poly += Integer.toString(exp[i]);
+                }
+            }
+            f.write(poly);
+            f.close();
+        } catch (IOException e) {
+            System.out.println("IOException");
+            e.printStackTrace();
+        }
+    }
+
+    public void print_poly() {
+        int len = coef.length;
+        if (coef == null || len == 0) {
+            System.out.print("Polynomial: 0");
+            System.out.println("");
+            return;
+        }
+        System.out.print("Polynomial: (" + coef[0] + ")x^" + exp[0]);
+        for (int i = 1; i < len; i++) {
+            System.out.print(" + (" + coef[i] + ")x^" + exp[i]);
+        }
+        System.out.println("");
     }
 }
